a716a91aa2b5463250e2442fc7d954240e75bf19
Sneh kheni <snehkheni54@gmail.com>
2026-02-17 23:01:50 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,7 @@
+print("hello world")
+student_name = "sneh"
+student_marks = 50
+print("my name is " + student_name + " and i got the " + str(student_marks) +" marks in my test")
+student_marks = 70.80
+student_name = "deep"
+print("my name is " + student_name + " and i got the " + str(student_marks) +" marks in my test")
