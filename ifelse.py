c29cc7872519ff5272f8ec9a600d6edb9a07b8ab
Sneh kheni <snehkheni54@gmail.com>
2026-02-17 23:02:22 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+c = input("enter a fucking number in here: ")
+d = int(input("enter your age: "))
+if c==str(5) or  d==10:
+    print("also write with and")
+    print("you are good")
+elif d==20:
+    print("you are decent ")
+else:
+    print("you are not good")
+
