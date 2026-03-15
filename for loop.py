a0d1a31b0f41cf9ca9802386bfcbc1f80bd4baff
Sneh kheni <snehkheni54@gmail.com>
2026-03-15 13:20:41 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,8 @@
+for letters in "sneh":
+    print(letters)
+movies  = ["it","thor","captain america","iron man"]
+for numbers in movies:
+    print(numbers)
+for move in range(len(movies)) :
+    print(movies[move])
+    #it means movies[0]
