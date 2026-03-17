aea45f9f54e04f366c9844b9850e19903e978aad
Sneh kheni <snehkheni54@gmail.com>
2026-03-17 19:30:32 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,13 @@
+def addition():
+    print("give to digits line by line")
+    b = input(":")
+    c = input(":")
+    e = int(b)+int(c)
+    print("your answer is "+ str(e))
+addition()
+
+def sayname(name,age):
+
+    print(name + age)
+sayname("sneh","23")
+sayname("ravi","23")
